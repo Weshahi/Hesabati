4144a52c8c1b6f8e8279e22d401c68dc7317a7e8
Weshahi <a.alweshahy@gmail.com>
2021-07-25 00:51:07 +0300

    add cart to routing

--- src/client/src/app/modules/pos/pos-routing.module.ts
+++ src/client/src/app/modules/pos/pos-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './components/cart/cart.component';
 import { PosComponent } from './pos.component';
 
 const routes: Routes = [
   {
     path: '',
     component: PosComponent
+  },
+  {
+    path: 'cart',
+    component: CartComponent
   }
 ];
 
