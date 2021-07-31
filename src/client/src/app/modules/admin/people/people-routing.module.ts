a32dfd3389d90c7e05f5c40db9202f1ff319c0e4
Weshahi <a.alweshahy@gmail.com>
2021-07-31 13:05:28 +0300

    remove system user and set it to users

--- src/client/src/app/modules/admin/people/people-routing.module.ts
+++ src/client/src/app/modules/admin/people/people-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PermissionGuard } from 'src/app/core/guards/permission.guard';
 import { CustomerComponent } from './components/customer/customer.component';
 import { SystemUserComponent } from './components/system-user/system-user.component';
 
@@ -11,7 +12,11 @@ const routes: Routes = [
   },
   {
     path: 'users',
-    component: SystemUserComponent
+    component: SystemUserComponent,
+    canActivate: [PermissionGuard],
+    data: {
+      allowedPermissions: ['Permissions.Users.View']
+    }
   }
 ];
 
