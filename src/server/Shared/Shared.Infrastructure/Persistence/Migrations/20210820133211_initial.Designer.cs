4c6b4a56cedbc9c5c0e26fdc729aca2c5a4aa0c9
Weshahi <a.alweshahy@gmail.com>
2021-08-20 17:55:36 +0300

    add my social accounts in about page

--- src/server/Shared/Shared.Infrastructure/Persistence/Migrations/20210820133211_initial.Designer.cs
+++ src/server/Shared/Shared.Infrastructure/Persistence/Migrations/20210820133211_initial.Designer.cs
@@ -4,14 +4,16 @@ using FluentPOS.Shared.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FluentPOS.Shared.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210820133211_initial")]
+    partial class initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
